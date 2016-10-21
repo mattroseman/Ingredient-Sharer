003b898302b0a3008bf76531806fd92650a7f795
Hun Jae Lee <hunjael@gmail.com>
2016-10-21 11:50:50 -0400

    properly redirect to sign-in page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'public#index'
+
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'devise/sessions#show', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
