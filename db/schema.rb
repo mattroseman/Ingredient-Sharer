a2b927da0e10b66731ecbfc10559684af3ca71ad
Hun Jae Lee <hunjael@gmail.com>
2016-10-25 14:28:57 -0400

    db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021174753) do
+ActiveRecord::Schema.define(version: 20161025180908) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -21,11 +21,12 @@ ActiveRecord::Schema.define(version: 20161021174753) do
     t.integer  "ttl",         limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.boolean  "is_request"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,              null: false
-    t.string   "encrypted_password",     limit: 255,              null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(version: 20161021174753) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "username",               limit: 255
+    t.string   "time_zone",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
