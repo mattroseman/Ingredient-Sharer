060fe57874f7e3a5325da4af1d797535ecc033d7
Hun Jae Lee <hunjael@gmail.com>
2016-10-26 12:09:07 -0400

    Change password requirements.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_reader :email, :password, :password_confirmation, :remember_me, :username
+
+  validate :password_complexity
+  def password_complexity
+    if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
+      errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one digit"
+    end
+  end
 end
