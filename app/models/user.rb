d7616b82b1903349623a53f0e5d168d972675449
Hun Jae Lee <hunjael@gmail.com>
2016-10-28 12:06:57 -0400

    merge & resolve conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_reader :email, :password, :password_confirmation, :remember_me, :username
+
+  # cheap fix found at https://github.com/phatworx/devise_security_extension/issues/54
+  def login_changed?
+    false
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,10 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  # Need 1 char of A-Z, a-z and 0-9
+  config.password_regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
