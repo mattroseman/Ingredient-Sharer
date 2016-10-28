8a4414d2c2f1b617d453cf374e3ce211caae963f
Matthew Roseman <mroseman95@gmail.com>
2016-10-28 18:28:41 -0500

    web-console is only added for development
    Also a basic rspec test has been created for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   it 'creates user successfully' do
-    foo = User.create!(email: "foo@bar.com", )
+    user = User.create(
+        email: 'test@example.com',
+        password: 'P@ssw0rd',
+        password_confirmation: 'P@ssw0rd'
+    )
+    expect(user.valid_password?('P@ssw0rd')).to be_truthy
   end
 end
