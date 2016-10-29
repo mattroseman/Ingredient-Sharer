d3e681755af0e6ded40095748e20bba681885855
Matthew Roseman <mroseman95@gmail.com>
2016-10-28 20:42:54 -0500

    fixed email can't be blank bug
    email can't be blank but when there is an email is fixed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
   # cheap fix found at https://github.com/phatworx/devise_security_extension/issues/54
   def login_changed?
     false
