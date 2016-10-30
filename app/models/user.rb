882670ce39a4a94df39a9f934b958222f96b57b9
Hun Jae Lee <hunjael@gmail.com>
2016-10-30 16:17:27 -0400

    fix sign-in issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,44 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, :uniqueness => true, :presence => {:message => 'Username cannot be blank'}, :uniqueness => { :case_sensitive => false }
+  validates :email, :uniqueness => true, :presence => {:message => 'Email cannot be blank'}
+
+  # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  # check format of username to not use email address of other user
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
   # cheap fix found at https://github.com/phatworx/devise_security_extension/issues/54
   def login_changed?
     false
   end
+
+  # enables user to sign in using their username OR email address
+  # found at: https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
 end
