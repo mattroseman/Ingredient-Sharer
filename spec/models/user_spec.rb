fc8bf91cb799f3d7937ffb57f60e665423c254ce
Hun Jae Lee <hunjael@gmail.com>
2016-11-03 14:55:21 -0400

    unit testing User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,96 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it 'creates user successfully' do
+  it 'creates user successfully (nominal case) with truthy password' do
     user = User.create(
+        username: 'someguy',
         email: 'test@example.com',
         password: 'P@ssw0rd',
         password_confirmation: 'P@ssw0rd'
     )
-    expect(user.valid_password?('P@ssw0rd')).to be_truthy
-  end
-  it "should reject incorrect email format with no (given) password" do
-    expect { User.create!(email: "incorrect_format", password: "P@ssw0rd") }.to raise_error
+
+    expect( user.valid_password?('P@ssw0rd') ).to be_truthy
   end
 
-  it "should reject password that does not include a special character" do
-    expect { User.create!(email: "correct@format.com", password: "Password123")}.to raise_error
+  it 'should reject incorrect email format with no (given) password' do
+    expect { User.create!(email: 'incorrect_format', password: 'P@ssw0rd') }.to raise_error
+  end
+
+  it 'should reject password that does not include a special character' do
+    expect { User.create!(email: 'correct@format.com', password: 'Password123') }.to raise_error
+  end
+
+  it 'should require username' do
+    u = User.create!(email: 'correct@email.com', password: 'P@ssw0rd!', username: 'someguy')
+    expect(u.username).to eq 'someguy'
+  end
+
+  it 'should reject a duplicate username' do
+    user = User.create(
+        username: 'someguy',
+        email: 'test@example.com',
+        password: 'P@ssw0rd',
+        password_confirmation: 'P@ssw0rd'
+    )
+
+    expect { User.create(
+        username: 'someguy',
+        email: 'another_test@example.com',
+        password: 'someP4SSW0RD',
+        password_confirmation: 'someP4SSW0RD'
+    ).save! }.to raise_error
+  end
+
+  it 'should reject a duplicate email' do
+    user = User.create(
+        username: 'someguy',
+        email: 'test@example.com',
+        password: 'P@ssw0rd',
+        password_confirmation: 'P@ssw0rd'
+    )
+
+    expect { same_email_user = User.create(
+        username: 'otherguy',
+        email: 'test@example.com',
+        password: 'someP4SSW0RD',
+        password_confirmation: 'someP4SSW0RD'
+    ).save! }.to raise_error
+  end
+
+  it 'should reject mismatching password confirmation pair' do
+    expect { User.create(
+        username: 'someguy',
+        email: 'some_guy@gmail.com',
+        password: 'P@ssw0rd',
+        password_confirmation: 'P@ssw0rd123'
+    ).save! }.to raise_error
+  end
+
+  it 'should not allow whitespace in username' do
+    expect { User.create(
+        username: 'some guy',
+        email: 'some_guy@gmail.com',
+        password: 'P@ssw0rd',
+        password_confirmation: 'P@ssw0rd'
+    ).save! }.to raise_error
+  end
+
+  it 'should be able to log in with email' do
+    user = User.create(
+        username: 'someguy',
+        email: 'some_guy@gmail.com',
+        password: 'P@ssw0rd',
+        password_confirmation: 'P@ssw0rd'
+    ).save!
+
+  end
+
+  it 'should be able to log in with username' do
+    user = User.create(
+        username: 'someguy',
+        email: 'some_guy@gmail.com',
+        password: 'P@ssw0rd',
+        password_confirmation: 'P@ssw0rd'
+    ).save!
   end
 end
