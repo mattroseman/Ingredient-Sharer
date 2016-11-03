a86e7303a9ea8935a969528eaeebadf6f405505e
Hun Jae Lee <hunjael@gmail.com>
2016-11-03 17:42:59 -0400

    remove obsolete timezone support method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   attr_accessor :login
 
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # this is required to redirect those who are not logged in.
+  # redirect those who are not logged in.
+  # set user's timezone
   before_filter :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -17,15 +17,6 @@ class ApplicationController < ActionController::Base
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  end
-
-
-  # sets the time zone for user
-  around_action :set_time_zone, if: :current_user
-
-  def set_time_zone(&block)
-    time_zone = current_user.time_zone || 'EST'
-    Time.use_zone(time_zone, &block)
   end
 
 end
