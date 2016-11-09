a6ba934bc95dee5bf02afe19a755bbfc20bdda53
Hun Jae Lee <hunjael@gmail.com>
2016-11-09 12:10:14 -0500

    added `user_id` attribute to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109170755) do
+ActiveRecord::Schema.define(version: 20161109170949) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161109170755) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.boolean  "is_request"
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
