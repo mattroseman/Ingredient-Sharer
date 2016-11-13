e931d101d410837c474db1c3bf091a8b4e24edc2
Hun Jae Lee <hunjael@gmail.com>
2016-11-12 19:38:08 -0500

    added admin

---
+++ app/models/admin.rb
@@ -0,0 +1,22 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # admins can also log in using username & email
+  attr_accessor :login
+
+  # overwrite devise's find_for_database_authentication method
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  # add validations for the username
+  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
+end
