dc741daa8c48574968a08a66ea3945d9c623b9aa
Matthew Roseman <mroseman95@gmail.com>
2016-11-18 05:09:09 -0500

    sign-up password regex working again
    closes #53

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :secure_validatable
 
   validates :username, :uniqueness => true, :presence => {:message => 'Username cannot be blank'}
   validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
