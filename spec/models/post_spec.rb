ec41fbb0c56791477c667f43fe0c8ef0cb45a55d
Hun Jae Lee <hunjael@gmail.com>
2016-11-18 00:31:58 +0000

    remove boilerplate spec message

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
   it 'creates post successfully (nominal case) with valid input' do
-    # user = Post.create()
+    post = Post.create()
   end
 end
