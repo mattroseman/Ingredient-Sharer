11285f5635cd95ef9aba98aea26c4ac934f2c3a8
Matthew Roseman <mroseman95@gmail.com>
2016-12-09 12:40:25 +0000

    updated data migration to better read in the test data, and to use the spam test data
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- db/migrate/20161117140041_populate_markov_models.rb
+++ db/migrate/20161117140041_populate_markov_models.rb
@@ -2,15 +2,18 @@ class PopulateMarkovModels < ActiveRecord::Migration
   def up
     #If the markov_models.sql file every needs to be regenerated, or another seed book is going to be used then uncomment and
     #rerun this merge. Otherwise load markov_models.sql outside of ruby.
-    ##
-    #File.open("sherlock.txt", "r") do |f|
+    #File.open("spam.txt", "r") do |f|
     #  prev_word = ""
     #  f.each_line do |line|
-    #    word_list = line.split
+    #    word_list = line.split(/\s+/)
     #    word_list.each do |word|
     #      word = word.gsub(/[^0-9A-Za-z']/, '')
-    #      execute ("INSERT INTO `markov_models` (word, next_word, count) VALUES (\"#{prev_word}\", \"#{word}\", 1) ON DUPLICATE KEY UPDATE count = count + 1;")
-    #      prev_word = word
+    #      if word.length > 30 or word.length < 1
+    #          next
+    #      else
+    #          execute ("INSERT INTO `markov_models` (word, next_word, count) VALUES (\"#{prev_word}\", \"#{word}\", 1) ON DUPLICATE KEY UPDATE count = count + 1;")
+    #          prev_word = word
+    #      end
     #    end
     #  end
     #end
